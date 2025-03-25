15f48f129fc69f53769569364bcac74a38d3842d
mhavill <mick@havill.me>
2025-03-26 09:44:43 +1100

    Template and initial additions

--- ESP/3_Let's_chat/Code-PIO/ESP32 Server/src/server.cpp
+++ ESP/3_Let's_chat/Code-PIO/ESP32 Server/src/server.cpp
@@ -1,4 +1,13 @@
+/*******************************
+ * Header
+ * Name: server.cpp
+ * Purpose: Sample Webserver
+ * Created Date: 26/03/2025
+ *******************************/
 
+/*******************************
+ * Includes
+ *******************************/
 #include <Arduino.h>
 
 #include <WiFi.h>
@@ -6,39 +15,46 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 
-#include "secrets.h"  
+#include "secrets.h"
+
+// Include the libraries we need for temperature
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+/*******************************
+ * Protptypes
+ *******************************/
+void handleRoot();
+void handleNotFound();
+void setup(void);
+
+/*******************************
+ * Definitions
+ *******************************/
 
 // const char* ssid = "MRPMG";
 // const char* password = "password";
 
+// TODO Add Code to read temperature
+// TODO Add Code to manage LED(s)
+
 WebServer server(80);
 
 const int led = 2;
+// DS1820 Data wire is plugged into GPIO 10 on the ESP32
+#define ONE_WIRE_BUS 10
 
-void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "hello from esp32!");
-  digitalWrite(led, 0);
-}
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
 
-void handleNotFound() {
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
-}
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
 
-void setup(void) {
+/*******************************
+ * Setup
+ *******************************/
+void setup(void)
+{
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
   Serial.begin(115200);
@@ -47,7 +63,8 @@ void setup(void) {
   Serial.println("");
 
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -57,15 +74,15 @@ void setup(void) {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("esp32")) {
+  if (MDNS.begin("esp32"))
+  {
     Serial.println("MDNS responder started");
   }
 
   server.on("/", handleRoot);
 
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
+  server.on("/inline", []()
+            { server.send(200, "text/plain", "this works as well"); });
 
   server.onNotFound(handleNotFound);
 
@@ -73,7 +90,43 @@ void setup(void) {
   Serial.println("HTTP server started");
 }
 
-void loop(void) {
+/*******************************
+ * Loop
+ *******************************/
+// TODO Remove blocking code
+void loop(void)
+{
   server.handleClient();
-  delay(2);//allow the cpu to switch to other tasks
+  delay(2); // allow the cpu to switch to other tasks
+}
+
+/*******************************
+ * Utility Functions
+ *******************************/
+
+// TODO Change to Neopixel
+void handleRoot()
+{
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from esp32!");
+  digitalWrite(led, 0);
+}
+
+void handleNotFound()
+{
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
 }
---
+++ ESP/3_Let's_chat/README.md
@@ -0,0 +1,16 @@
+# Let's Chat -ESP32
+
+## ESP32 Server
+
+We are going to set up an ESP32 with a couple of functions as a web server.
+
+This incldes using [mDNS](https://docs.espressif.com/projects/esp-protocols/mdns/docs/latest/en/index.html) to give the device a memorable name and save us from needing the IP address
+
+The initial functions will use [HTTP Methods](https://www.w3schools.com/tags/ref_httpmethods.asp):
+
+GET to get the current temperature measured on the [Dallas DS1820](https://www.electronicwings.com/esp32/ds18b20-sensor-interfacing-with-esp32)
+
+PUT to change the [Neopixel LED](https://core-electronics.com.au/guides/ws2812-addressable-leds-arduino-quickstart-guide/)
+
+You can use a device browser to send the HTTP to test the responses.
+
