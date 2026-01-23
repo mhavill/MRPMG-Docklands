bd31eb7a4f095e521f8e64172ff347a20e3fa32d
mhavill <mick@havill.me>
2026-01-24 08:06:27 +1100

    tidy up

--- ESP/3_Let's_chat/Code-PIO/ESP32 Server/src/server.cpp
+++ ESP/3_Let's_chat/Code-PIO/ESP32 Server/src/server.cpp
@@ -272,19 +272,9 @@ bool wmloop(void *)
     // reset settings - for testing
     wm.resetSettings();
 
-    // set configportal timeout
-    // wm.setConfigPortalTimeout(timeout);
 
-    // if (!wm.startConfigPortal("OnDemandAP")) {
-    //   Serial.println("failed to connect and hit timeout");
-    //   delay(3000);
-    //   //reset and try again, or maybe put it to deep sleep
     ESP.restart();
-    //   delay(5000);
-    // }
 
-    // if you get here you have connected to the WiFi
-    // Serial.println("connected...yeey :)");
   }
   return true;
 }
